db22384a3b35712cb62bbcc974b8d08675271588
Yahir Hernández <118926867+HernandezYahir@users.noreply.github.com>
2023-01-13 14:50:43 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Mi Práctica Con Codesandbox</h1>
+<h2>Yahir Antonio Hernández Escamilla</h2>
+<div>
+  Universidad Tecnológica de Durango
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">My Github</a>.
+</div>
+`;
